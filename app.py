700bcdef94b2303537517b5480b69584805af694
LeoDellAmatrice <Leonardo.dellamatrice@gmail.com>
2025-11-05 16:32:16 -0300

    rota /api/login

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import database.produtos.produtos_views as produtos_views
 import database.categorias_views.categorias_views as categorias_views
+import database.login.login_views as login_views
 
 app = Flask(__name__)
 CORS(app)
@@ -17,7 +18,7 @@ def api_login():
     except KeyError:
         return jsonify({'error': 'dados invalidos'})
 
-    return True
+    return jsonify({'successo': login_views.verificar_usuario_db(email, senha)})
 
 
 @app.route('/api/produtos')
